4bd75008cacb529a0ddfe0d93cecb392aacc90dd
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-10-24 14:53:11 -0600

    11 - Handling Secrets with Python Dotenv

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o_vb6ld)b8wd7ckv(gv^s-800=68u*wab84obl^(#^qvyg##%0"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (
+    str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
+)  # config("DJANGO_DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
