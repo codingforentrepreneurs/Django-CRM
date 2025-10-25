574a2624520153f9f1ec8b7b7f1f4706c702356e
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-10-25 13:08:30 -0600

    15 - Django View to Valid HTML

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -4,8 +4,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 # request -> /dashboard -> django -> urls.py -> view -> response
+# request -> html
+# request -> json -> api
+# request -> ai agent -> mcp
+# request -> xml -> podcast feeds
 def dashboard_webpage(request, *args, **kwargs):
     print(request.user, request.user.is_authenticated)
     if not request.user.is_authenticated:
         return redirect("/auth/google/login/")
-    return HttpResponse(f"Hi {request.user}")
+    html = "<h1 style='color:red'>Hello World</h1>"
+    html = """
+    <!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold text-red-500">
+      Hello world!
+    </h1>
+  </body>
+</html>
+"""
+    return HttpResponse(html)
