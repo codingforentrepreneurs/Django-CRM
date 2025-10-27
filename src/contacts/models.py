fd9c7761bad0f92a5f9c984612af81078675f2ca
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-10-27 16:12:18 -0600

    29 - Data Ownership through Foreign Keys

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -7,9 +7,18 @@ User = settings.AUTH_USER_MODEL  # -> "auth.User"
 # user_id => 1
 # Create your models here.
 class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user -> Contact.objects.filter(user=request.user)
+    # user.modelname_set.all()
+    # user.contact_set.all()
+    # default related name = contact_set
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="mycontacts"
+    )  # user.mycontacts.all()
     email = models.EmailField()
     notes = models.TextField(blank=True, default="")
+    last_edited_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name="my_contact_edits"
+    )  # user.my_contact_edits.all()
 
     def get_absolute_url(self):
         return f"/contacts/{self.id}/"
--- src/events/models.py
+++ src/events/models.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 # from contacts.models import Contact
+
+User = settings.AUTH_USER_MODEL
 
 
 class Event(models.Model):
@@ -10,9 +13,17 @@ class Event(models.Model):
         CREATED = "created", "Create Event"
         VIEWED = "viewed", "View Event"
 
+    user = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.SET_NULL,
+        help_text="Performed by user",
+        related_name="myevents",
+    )
     type = models.CharField(
         max_length=40, default=EventType.VIEWED, choices=EventType.choices
     )
+    # foreign key = tight relationship
     object_id = models.IntegerField(blank=True, default=-1)
     model_name = models.CharField(max_length=120, default="contacts.content")
     timestamp = models.DateTimeField(auto_now_add=True)
