fbb9d3d3c87adff6d4d6f5514e0de51f3ac58230
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-10-28 17:18:24 -0600

    38 - Counting Event Types in Time Ranges

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -10,10 +10,6 @@ User = settings.AUTH_USER_MODEL  # -> "auth.User"
 # user_id => 1
 # Create your models here.
 class Contact(models.Model):
-    # user -> Contact.objects.filter(user=request.user)
-    # user.modelname_set.all()
-    # user.contact_set.all()
-    # default related name = contact_set
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="mycontacts"
     )  # user.mycontacts.all()
@@ -23,6 +19,8 @@ class Contact(models.Model):
         User, on_delete=models.SET_NULL, null=True, related_name="my_contact_edits"
     )  # user.my_contact_edits.all()
     events = GenericRelation(Event)  # contact_instance.events.all()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return f"/contacts/{self.id}/"
