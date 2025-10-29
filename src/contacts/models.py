199c9173df36c2cf3edaef474885123a69eaebac
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-10-29 14:22:38 -0600

    47 - Parse Google API Results

--- src/contacts/models.py
+++ src/contacts/models.py
@@ -13,7 +13,9 @@ class Contact(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="mycontacts"
     )  # user.mycontacts.all()
-    email = models.EmailField()
+    email = models.EmailField(db_index=True)
+    first_name = models.CharField(max_length=120, default="", blank=True)
+    last_name = models.CharField(max_length=120, default="", blank=True)
     notes = models.TextField(blank=True, default="")
     last_edited_by = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, related_name="my_contact_edits"
